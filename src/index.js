c4cb6fec38350515695af357fefc68cb276cb0fa
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-12-11 19:00:22 +0100

    Included Swagger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 const port = process.env.PORT || 3000;
+
+const swaggerDoc = YAML.load('./swagger.yaml');
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.get('/', (req, res) => {
     console.log("SmartHome App");
