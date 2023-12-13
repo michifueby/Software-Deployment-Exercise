e69f8931c99c9ebd2ecac3c3a2b89938da94daf4
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-12-13 15:19:24 +0100

    Finalized App

---
+++ src/assets/script.js
@@ -0,0 +1,12 @@
+
+function getDoc() {
+    window.open('/doc', '_self');
+}
+
+function getSecurityStatus() {
+    window.open('/security', '_self');
+}
+
+function getTemperature() {
+    window.open('/temperature', '_self');
+}
